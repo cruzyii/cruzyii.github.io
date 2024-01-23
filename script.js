5bc78d78e7bf6e92bcd30aea10b6e86c012f5877
cruzyii <140167709+cruzyii@users.noreply.github.com>
2024-01-23 12:09:26 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) => {
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        else{
+           entry.target.classList.remove('show'); 
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+//Opening, closing modals
+
 const openPopupButtons = document.querySelectorAll('[data-popup-target]')
 const closePopupButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
 
-openPopupButtons.forEach(button => {
+openPopupButtons.forEach(button => { //Listens for button click, calls openPopup
     button.addEventListener('click', () => {
         const popup = document.querySelector(button.dataset.popupTarget)
         openPopup(popup)
     })
 })
 
-closePopupButtons.forEach(button => {
+closePopupButtons.forEach(button => { //Listens for button click, calls closePopup
     button.addEventListener('click', () => {
         const popup = button.closest('.popup')
         closePopup(popup)
@@ -29,6 +46,7 @@ function closePopup(popup){
 }
 
 
+//Storing user input to username variable, input validation, opening popupWelcome
 let username;
 let checkBox = document.getElementById("checkbox");
 let welcomeHeading = document.getElementById("headingWelcome");
@@ -36,12 +54,11 @@ let welcomeHeading = document.getElementById("headingWelcome");
 
 document.getElementById("submit").onclick = function(){
     if(checkBox.checked == true){
-        username = document.getElementById("name").value;
-
         if(document.getElementById('name').value == ""){
             alert("Enter your name!");
         }
         else{
+            username = document.getElementById("name").value;
             closePopup(popup)
             welcomeHeading.innerText  = "Welcome, " + username;
             openPopup(popupWelcome)
